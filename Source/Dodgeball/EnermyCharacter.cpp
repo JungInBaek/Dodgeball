fcd139d2931069229ecddf1cf739402e7aa6c29d
unknown <bji0519@gmail.com>
2025-03-10 08:55:41 +0900

    Feat: 적 캐릭터 생성
    - CanSeeActor(타겟 감지 가능 여부) 함수 구현

--- Source/Dodgeball/EnermyCharacter.cpp
+++ Source/Dodgeball/EnermyCharacter.cpp
@@ -2,6 +2,7 @@
 
 
 #include "EnermyCharacter.h"
+#include "Engine/World.h"
 
 // Sets default values
 AEnermyCharacter::AEnermyCharacter()
@@ -30,5 +31,42 @@ void AEnermyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AEnermyCharacter::LookAtActor(AActor* TargetActor)
+{
+}
+
+bool AEnermyCharacter::CanSeeActor(const AActor* TargetActor) const
+{
+	if (TargetActor == nullptr)
+	{
+		return false;
+	}
+
+	// 라인 트레이스 결과 저장
+	FHitResult Hit;
+
+	// 라인 트레이스 시작 위치
+	FVector Start = GetActorLocation();
+
+	// 라인 트레이스 끝 위치
+	FVector End = TargetActor->GetActorLocation();
+
+	// 시야 판단을 위한 트레이스 채널
+	ECollisionChannel Channel = ECollisionChannel::ECC_Visibility;
+
+	FCollisionQueryParams QueryParams;
+
+	// 라인 트레이스를 실행하는 액터 무시
+	QueryParams.AddIgnoredActor(this);
+
+	// 타겟 액터 무시
+	QueryParams.AddIgnoredActor(TargetActor);
+
+	// 라인 트레이스 실행
+	GetWorld()->LineTraceSingleByChannel(Hit, Start, End, Channel, QueryParams);
+
+	return Hit.bBlockingHit;
 }
 
